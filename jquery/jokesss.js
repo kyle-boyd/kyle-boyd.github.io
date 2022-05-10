d8b04f4450ca3ef666472663d0134fdea4135472
kyle-boyd <kyle.boyd@gmail.com>
2022-05-09 22:39:13 -0600

    upload

---
+++ jquery/jokesss.js
@@ -0,0 +1,138 @@
+
+
+        $(document).ready(function(){
+          var joke1List = ["Hello", "Hallo", "Howdy"];
+          var answer1List = ["Hello", "Hallo", "Howdy"];
+          var buttonClass = "blank"
+
+          var fullJokeList = [];
+
+
+            var jokeFilter = ["jokes"];
+            var jokeList = filterByProperty(fullJokeList, jokeFilter);
+
+
+
+
+// Get joke data
+
+var fullJokeList;
+	$.ajax({
+	  type: "GET",
+    url: "jokesss.csv",
+    dataType: "text",
+	  success: function(response)
+	  {		fullJokeList = $.csv.toArrays(response);
+
+    }
+
+	   });
+
+//Responsive Design
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+  //Sort Buttons
+            $('button').click(function(){
+
+              if(this.value=="sort") {
+              //toggle appearance
+              $(this).toggleClass('button-sort button-sort-off');
+
+              //replace image
+              $('img',this).toggle();
+
+              //update jokeFilter
+
+              var filterIndex = $.inArray(this.id, jokeFilter);
+
+              if(filterIndex==-1){
+                jokeFilter.push(this.id);
+              }  else{
+                jokeFilter.splice(filterIndex, 1);
+              }
+            }
+
+
+              var jokeList = filterByProperty(fullJokeList, jokeFilter);
+
+
+            });
+
+
+            // Next Jokes
+
+                        $(".Generate").click(function(){
+                          var jokeList = filterByProperty(fullJokeList, jokeFilter);
+                          var jokeNum = Math.floor(Math.random() * jokeList.length);
+                          
+                          var joke1 = jokeList[jokeNum][1]
+                          var answer1 = jokeList[jokeNum][2]
+                          var displayjoke = "<p>" + joke1 + "</b>" + answer1 + "</p>" ;
+                          $("#joke1").html("<p>" + joke1 + "</p>");
+                          $("#answer1").html("<p>" + answer1 + "</p>");
+
+                          });
+        });
+
+
+
+
+
+
+
+        //sliding navbar
+
+                $(function(){
+                    $('.icon').click(function(){
+                        $('.sliding-navbar').toggleClass('sliding-navbar--open');
+                      //  $('.mask').toggleClass('show');
+                        $('.mask').fadeIn(1000);
+                        //$('.mask').css('z-index', 200);
+                        //$('.mask').css('opacity', .25);
+                        $('.hamburger').toggleClass('menu-opened');
+                    });
+
+                    $('.mask').click(function(){
+                        $('.sliding-navbar').toggleClass('sliding-navbar--open');
+                        $('.mask').fadeOut();
+                      //  $('.mask').delay(0.8s).css('z-index', -100);
+
+                        $('.hamburger').toggleClass('menu-opened');
+                    })
+                });
+
+// filter
+                function filterByProperty(array, array2){
+                    var filtered = [];
+                    for (var j = 0; j < array2.length; j++){
+                      for(var i = 0; i < array.length; i++){
+                      if (array[i][0]== array2[j]){
+                        filtered.push(array[i])
+
+                      }
+
+                    }
+                  }
+                    return filtered;
+
+                }
+
+// check empties
+
+                function checkEmpty(array){
+                    var newLength = 0;
+
+                      for(var i = 0; i < array.length; i++){
+                      if (array[i]=! "") newLength = newLength+1;
+                      }
+                        return newLength;
+                    }
